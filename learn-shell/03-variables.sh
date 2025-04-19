e0021f7c2710e8ea9699c4412df5a28900600310
Richard Okoro <glasshouse747@gmail.com>
2025-04-19 17:24:04 +0100

    test-bash & systemd files

--- learn-shell/03-variables.sh
+++ learn-shell/03-variables.sh
@@ -11,3 +11,15 @@ y=Hello
 echo x - $x
 echo x - ${x}
 
+
+# Variables are used to avoid repetition. Meaning if we have some data that needs to be used in many places in a the script, we declare that as a variable
+# then will access that variable. Any situation the data needs to be changed, we change in one place, it changes in all places (where it appears).
+
+# Naming Convention for variables
+# Alphabets, a-z, A-Z, numbers, underscore
+
+#Variable Types
+#Shell does not have any variable type by default
+# Every variable is a string. Not in, long characters, booleans
+
+#Input Variables
