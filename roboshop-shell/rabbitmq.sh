82691b6343af697376cd692d1caf916aaa474ec7
Richard Okoro <glasshouse747@gmail.com>
2025-04-20 17:10:16 +0100

    test-bash & systemd files

--- roboshop-shell/rabbitmq.sh
+++ roboshop-shell/rabbitmq.sh
@@ -1,6 +1,8 @@
+component_name=rabbitmq-server
+source common.sh
+
 cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
 dnf install rabbitmq-server -y
-systemctl enable rabbitmq-server
-systemctl start rabbitmq-server
+systemd_setup
 rabbitmqctl add_user roboshop roboshop123
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
