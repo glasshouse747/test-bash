6f7abb7da88bdb47840d314d8c78b14a923c0b71
Richard Okoro <glasshouse747@gmail.com>
2025-04-23 16:06:01 +0100

    test-bash & systemd files

--- roboshop-shell/mysql.sh
+++ roboshop-shell/mysql.sh
@@ -1,13 +1,6 @@
-- name: Install MySQL Server
-  ansible.builtin.dnf:
-    name: mysql-sever
-    state: latest
+component_name=mysqld
+source common.sh
 
-- name: Start MySQL
-  ansible.builtin.systemd_service:
-    name: mysqld
-    state: latest
-    enabled: yes
-
-- name: Setup MySQL Root Password
-  ansible.builtin.shell: mysql_secure_installation --set-root-pass RoboShop@1
+dnf install mysql-server -y
+systemd_setup
+mysql_secure_installation --set-root-pass RoboShop@1
