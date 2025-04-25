302b31aa2490d8261b1788100678486621984821
Richard Okoro <glasshouse747@gmail.com>
2025-04-25 19:06:16 +0100

    test-bash & systemd files

--- roboshop-terraform/main.tf
+++ roboshop-terraform/main.tf
@@ -21,17 +21,6 @@ resource "azurerm_network_security_group" "shared-nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
-  security_rule {
-    name                       = "allow-ssh"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
 
   security_rule {
     name                       = "allow-http"
