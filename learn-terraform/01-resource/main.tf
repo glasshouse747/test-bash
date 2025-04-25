8e7a71cf69d155cd05b06c56bd58299464fe8497
Richard Okoro <glasshouse747@gmail.com>
2025-04-25 15:03:18 +0100

    test-bash & systemd files

--- learn-terraform/01-resource/main.tf
+++ learn-terraform/01-resource/main.tf
@@ -3,16 +3,18 @@ provider "azurerm" {
 }
 
 resource "azurerm_virtual_machine" "test" {
-  name                  = "test-vm"
-  location              = "UK West"
-  resource_group_name   = "my-first-rg"
-  network_interface_ids = ["/subscriptions/eb986b09-9743-4aa1-b10f-53da04d8708c/resourceGroups/my-first-rg/providers/Microsoft.Network/networkInterfaces/terraform-testing"]
-  vm_size               = "Standard_B2s"
+  name                = "test-vm"
+  location            = "UK West"
+  resource_group_name = "my-first-rg"
+  network_interface_ids = [
+    "/subscriptions/eb986b09-9743-4aa1-b10f-53da04d8708c/resourceGroups/my-first-rg/providers/Microsoft.Network/networkInterfaces/terraform-testing"
+  ]
+  vm_size             = "Standard_B2s"
 
   delete_os_disk_on_termination = true
 
   storage_image_reference {
-    id ="/subscriptions/eb986b09-9743-4aa1-b10f-53da04d8708c/resourceGroups/my-first-rg/providers/Microsoft.Compute/images/local-devops-practice"
+    id = "/subscriptions/eb986b09-9743-4aa1-b10f-53da04d8708c/resourceGroups/my-first-rg/providers/Microsoft.Compute/images/local-devops-practice"
   }
 
   storage_os_disk {
@@ -27,3 +29,4 @@ resource "azurerm_virtual_machine" "test" {
     admin_username = "azuser"
     admin_password = "Giveme123456"
   }
+}
