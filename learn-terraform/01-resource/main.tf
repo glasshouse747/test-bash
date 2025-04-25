3e40066cd37eb84cdef70c5ef07fd504776bc353
Richard Okoro <glasshouse747@gmail.com>
2025-04-25 15:15:48 +0100

    test-bash & systemd files

--- learn-terraform/01-resource/main.tf
+++ learn-terraform/01-resource/main.tf
@@ -17,17 +17,18 @@ resource "azurerm_virtual_machine" "test" {
   storage_image_reference {
     id = "/subscriptions/eb986b09-9743-4aa1-b10f-53da04d8708c/resourceGroups/my-first-rg/providers/Microsoft.Compute/images/local-devops-practice"
   }
-
   storage_os_disk {
     name              = "test-vm"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
   }
-
   os_profile {
     computer_name  = "test-vm"
     admin_username = "azuser"
     admin_password = "Giveme123456"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
   }
 }
