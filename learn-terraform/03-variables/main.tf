92443cb50b90055d14ddb33690b49bf99b61e873
Richard Okoro <glasshouse747@gmail.com>
2025-04-28 13:31:38 +0100

    test-bash & systemd files

--- learn-terraform/03-variables/main.tf
+++ learn-terraform/03-variables/main.tf
@@ -85,10 +85,10 @@ variable "v1" {}
   output "v1" {
     value = var.v1
   }
-variable "env" {}
-output "env" {
-  value = var.env
-}
+# variable "env" {}
+# output "env" {
+#   value = var.env
+# }
 
 variable "server_count" {}
 output "server_count" {
