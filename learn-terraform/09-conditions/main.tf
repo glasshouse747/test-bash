a8512675f3e1f97257b2ec9dc73adc15f7f437f4
Richard Okoro <glasshouse747@gmail.com>
2025-04-29 12:35:12 +0100

    test-bash & systemd files

--- learn-terraform/09-conditions/main.tf
+++ learn-terraform/09-conditions/main.tf
@@ -1,5 +1,35 @@
-resource "null_resource" "test" {}
+# resource "null_resource" "test" {}
 
-output "test" {
-  value = length(null_resource.test)
+# output "test" {
+#   value = length(null_resource.test)
+# }
+#
+
+provider "azurerm" {
+  features {}
+  subscription_id = "eb986b09-9743-4aa1-b10f-53da04d8708c"
 }
+
+variable "nodes" {
+  default = {
+    test2 = {
+      private_ip_address_allocation = "Static"
+    }
+    test1 = {
+    }
+  }
+}
+
+resource "azurerm_network_interface" "privateip" {
+  for_each            = var.nodes
+  name                = "test-ip"
+  location            = "UK West"
+  resource_group_name = "my-first-rg"
+
+  ip_configuration {
+    name                          = "test-ip"
+    subnet_id                     = "/subscriptions/eb986b09-9743-4aa1-b10f-53da04d8708c/resourceGroups/my-first-rg/providers/Microsoft.Network/virtualNetworks/test-network/subnets/default"
+    private_ip_address_allocation = length(each.value["private_ip_address_allocation"]) > 0 ? each.value["private_ip_address_allocation"] : "Dynamic"
+  }
+}
+
