731de8dacdb87564997f61d89b1c9b1def0599f7
Richard Okoro <glasshouse747@gmail.com>
2025-04-29 13:23:00 +0100

    test-bash & systemd files

---
+++ learn-terraform/10-data/main.tf
@@ -0,0 +1,11 @@
+data "azurerm_resource_group" "example" {
+  name = "my-first-rg"
+}
+
+output "id" {
+  value = data.azurerm_resource_group.example.id
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "eb986b09-9743-4aa1-b10f-53da04d8708c"
