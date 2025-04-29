cb7c61a0b51c812e672a86d0fa3262aa6ed263eb
Richard Okoro <glasshouse747@gmail.com>
2025-04-29 11:22:13 +0100

    test-bash & systemd files

---
+++ learn-terraform/06-loops-for-each/main.tf
@@ -0,0 +1,57 @@
+
+provider "azurerm" {
+  features {}
+  subscription_id = "eb986b09-9743-4aa1-b10f-53da04d8708c"
+}
+
+variable "nodes" {
+  default = {
+    test1 = {
+      vm_size = "Standard_B2s"
+    }
+    test2 = {
+      vm_size = "Standard_B2s"
+    }
+  }
+}
+resource "azurerm_network_interface" "privateip" {
+  for_each            = var.nodes
+  name                = "${each.key}-ip"
+  location            = "UK West"
+  resource_group_name = "my-first-rg"
+
+  ip_configuration {
+    name                          = "${each.key}-ip"
+    subnet_id                     = "/subscriptions/eb986b09-9743-4aa1-b10f-53da04d8708c/resourceGroups/my-first-rg/providers/Microsoft.Network/virtualNetworks/test-network/subnets/default"
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_virtual_machine" "test" {
+  for_each            = var.nodes
+  name                = "${each.key}-vm"
+  location            = "UK West"
+  resource_group_name = "my-first-rg"
+  network_interface_ids = [azurerm_network_interface.privateip[each.key].id]
+  vm_size             = "Standard_B2s"
+
+  delete_os_disk_on_termination = true
+
+  storage_image_reference {
+    id = "/subscriptions/eb986b09-9743-4aa1-b10f-53da04d8708c/resourceGroups/my-first-rg/providers/Microsoft.Compute/images/local-devops-practice"
+  }
+  storage_os_disk {
+    name              = "${each.key}-disk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "${each.key}-vm"
+    admin_username = "azuser"
+    admin_password = "Giveme123456"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+}
