ed1468eb9475a83d68227cb95cc32f0df1b3459d
Richard Okoro <glasshouse747@gmail.com>
2025-04-29 10:55:26 +0100

    test-bash & systemd files

--- learn-terraform/06-loops/main.tf
+++ learn-terraform/06-loops/main.tf
@@ -19,3 +19,53 @@ resource "null_resource" "testx" {
 output "testx" {
   value = null_resource.testx.id
 }
+
+## Count is not a great one to start for looping, as it has its own problems
+
+variable "nodes" {
+  default = [
+  "test1",
+  "test2"]
+}
+
+resource "azurerm_network_interface" "privateip" {
+  count               = length(var.nodes)
+  name                = "${var.nodes[count.index]}-ip"
+  location            = "UK West"
+  resource_group_name = "my-first-rg"
+
+  ip_configuration {
+    name                          = "${var.nodes[count.index]}-ip"
+    subnet_id                     = "/subscriptions/eb986b09-9743-4aa1-b10f-53da04d8708c/resourceGroups/my-first-rg/providers/Microsoft.Network/virtualNetworks/test-network/subnets/default"
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_virtual_machine" "test" {
+  count               = length(var.nodes)
+  name                = "${var.nodes[count.index]}-vm"
+  location            = "UK West"
+  resource_group_name = "my-first-rg"
+  network_interface_ids = "/subscriptions/eb986b09-9743-4aa1-b10f-53da04d8708c/resourceGroups/my-first-rg/providers/Microsoft.Network/networkInterfaces/test"
+  vm_size             = "Standard_B2s"
+
+  delete_os_disk_on_termination = true
+
+  storage_image_reference {
+    id = "/subscriptions/eb986b09-9743-4aa1-b10f-53da04d8708c/resourceGroups/my-first-rg/providers/Microsoft.Compute/images/local-devops-practice"
+  }
+  storage_os_disk {
+    name              = "${var.nodes[count.index]}-disk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "${var.nodes[count.index]}-vm"
+    admin_username = "azuser"
+    admin_password = "Giveme123456"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+}
