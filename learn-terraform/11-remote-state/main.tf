a54469a83ef3b68a30efd37540c528443e07b293
Richard Okoro <glasshouse747@gmail.com>
2025-05-01 14:46:13 +0100

    test-bash & systemd files

--- learn-terraform/11-remote-state/main.tf
+++ learn-terraform/11-remote-state/main.tf
@@ -1,12 +1,30 @@
-resource "null_resource" "test" {}
+# resource "null_resource" "test" {}
+#
+# terraform {
+#   backend "azurerm" {
+#     use_cli              = true
+#     subscription_id      = "eb986b09-9743-4aa1-b10f-53da04d8708c"
+#     resource_group_name  = "my-first-rg"
+#     storage_account_name = "mydevopshop"
+#     container_name       = "roboshop-state-files"
+#     key                  = "test.terraform.tfstate"
+#   }
+# }
 
-terraform {
-  backend "azurerm" {
-    use_cli              = true
-    subscription_id      = "eb986b09-9743-4aa1-b10f-53da04d8708c"
-    resource_group_name  = "my-first-rg"
-    storage_account_name = "mydevops"
-    container_name       = "roboshop-state-files"
-    key                  = "test.terraform.tfstate"
-  }
+resource "azurerm_resource_group" "tfstate" {
+  name     = "my-first-rg"
+  location = "UK West"
+}
+
+resource "azurerm_storage_account" "tfstate" {
+  name                     = "mydevopshop"
+  resource_group_name      = my-first-rg
+  location                 = "UK West"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "roboshop-state-files"
+  container_access_type = "private"
 }
