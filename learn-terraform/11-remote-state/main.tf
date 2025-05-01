9fc3f44fbbf27ac01c982e903d5b686c9fd3f16d
Richard Okoro <glasshouse747@gmail.com>
2025-05-01 14:42:09 +0100

    test-bash & systemd files

--- learn-terraform/11-remote-state/main.tf
+++ learn-terraform/11-remote-state/main.tf
@@ -5,7 +5,7 @@ terraform {
     use_cli              = true
     subscription_id      = "eb986b09-9743-4aa1-b10f-53da04d8708c"
     resource_group_name  = "my-first-rg"
-    storage_account_name = "mydevopshop"
+    storage_account_name = "mydevops"
     container_name       = "roboshop-state-files"
     key                  = "test.terraform.tfstate"
   }
