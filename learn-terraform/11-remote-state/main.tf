17691c8f8ebaaa4a7d9bd9040407154955bca709
Richard Okoro <glasshouse747@gmail.com>
2025-05-01 14:29:43 +0100

    test-bash & systemd files

--- learn-terraform/11-remote-state/main.tf
+++ learn-terraform/11-remote-state/main.tf
@@ -3,7 +3,6 @@ resource "null_resource" "test" {}
 terraform {
   backend "azurerm" {
     use_cli              = true
-    # tenant_id            = "00000000-0000-0000-0000-000000000000"
     subscription_id      = "eb986b09-9743-4aa1-b10f-53da04d8708c"
     resource_group_name  = "my-first-rg"
     storage_account_name = "mydevopshop"
