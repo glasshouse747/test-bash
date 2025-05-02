fc989eb8fe3b425e7ae4e7ae9d76fc343515b744
Richard Okoro <glasshouse747@gmail.com>
2025-05-02 19:40:19 +0100

    test-bash & systemd files

--- learn-terraform/12-hashi-vault/main.tf
+++ learn-terraform/12-hashi-vault/main.tf
@@ -14,7 +14,7 @@ data "vault_generic_secret" "secret" {
 #   filename = "/tmp/vault"
 # }
 
-resource "local_file" "foo" {
+resource "local_file" "foo1" {
   content  = data.vault_generic_secret.secret.data_json["password"]
   filename = "/tmp/vault-pass"
 }
