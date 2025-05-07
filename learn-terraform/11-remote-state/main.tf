f1443fdb43fedaa8e67becef66ae72687b1f1b66
Richard Okoro <glasshouse747@gmail.com>
2025-05-07 12:44:09 +0100

    roboshop x

--- learn-terraform/11-remote-state/main.tf
+++ learn-terraform/11-remote-state/main.tf
@@ -3,19 +3,6 @@ provider "azurerm" {
   features {}
 }
 
-# resource "null_resource" "test" {}
-#
-# terraform {
-#   backend "azurerm" {
-#     use_cli              = true
-#     subscription_id      = "eb986b09-9743-4aa1-b10f-53da04d8708c"
-#     resource_group_name  = "my-first-rg"
-#     storage_account_name = "mydevopshop"
-#     container_name       = "roboshop-state-files"
-#     key                  = "test.terraform.tfstate"
-#   }
-# }
-
 resource "azurerm_storage_account" "tfstorage" {
   name                     = "mydevopshop"
   resource_group_name      = "my-first-rg"
